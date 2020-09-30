4f33fa7874cdfaf1fdae101ab911334046484d09
Cedric Don Coloma <colomce@oocl.com>
2020-09-30 20:49:00 +0800

    Sort numeric array in descending order.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ console.log(colors.join(','));
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+console.log(a.sort(function(a, b){return b - a}));
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
